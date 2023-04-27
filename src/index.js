de8e51cf8ff8799784ecf6d3380c9d03a7a4452a
Kosmos <3apa3a.iickov@gmail.com>
2023-04-28 02:24:50 +0300

    feat(component): fix parcel, fix structure, create components input, button, errors, forms, start create chat (sprint_1)

--- src/index.js
+++
@@ -1,4 +0,0 @@
-import './styles/index.css';
-import app from './app.hbs'
-
-document.body.innerHTML = app({page : `<div class="page__index">hello</div>`});
--- src/pages/main/index.js
+++
@@ -1,8 +0,0 @@
-import template from './index.hbs';
-import app from './../../app.hbs';
-import  { links } from '../../mocks';
-
-document.body.innerHTML = app(
-    {
-        page : template({links: links}),
-    });
