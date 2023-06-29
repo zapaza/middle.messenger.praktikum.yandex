36bf92273dd43be649714e19c57d4edb05867749
Kosmos <3apa3a.iickov@gmail.com>
2023-06-29 09:49:53 +0300

    fix(server): fix server file

--- server/server.js
+++ server/server.js
@@ -1,8 +1,15 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.static(`./dist/`));
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/dist/index.html`);
+});
+app.get('*', (req, res) => {
+  res.sendFile(`${__dirname}/dist/index.html`);
+});
 app.listen(PORT, function () {
-    console.log(`app listening on port ${PORT}!`);
+  console.log(`app listening on port ${ PORT }!`);
 });
